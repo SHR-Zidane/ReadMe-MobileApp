29c72f55b9001515540168b88cfc453ee0f721bb
SHR-Zidane <pd01wvj@eduvaud.ch>
2026-04-29 15:23:17 +0200

    feat(models): création du model epubbook

---
+++ ReadMe/server/controllers/epubController.js
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import db from '../db.js';
+
+const Epub = db.define('Epub', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  file: {
+    type: DataTypes.BLOB('long'),
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+export default Epub;
